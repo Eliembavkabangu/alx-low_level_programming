163652738590e35dba34d07598f9f697e8dca951
Eliembavkabangu <mkabanguflo@gmail.com>
2022-07-05 11:36:18 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,11 @@
+/**
+* print_alphabet -> prints the lowercase alphabets
+*/
 void print_alphabet(void)
+ii{
 for (char letter = 'a'; letter <= 'z'; letter++)
+{
 _putchar(letter);
+}
 _putchar("\n");
+}
