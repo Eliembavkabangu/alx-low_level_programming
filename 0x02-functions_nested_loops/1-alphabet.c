1ba056cb50c8b59ede10995696ef8c92e69e2c1a
Eliembavkabangu <mkabanguflo@gmail.com>
2022-07-05 10:24:31 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+print_alphabet();
+
+return (0);
+}
+void print_alphabet(void)
+for (char letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar("\n");
